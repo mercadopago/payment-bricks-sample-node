f207fac06685bfde45e45285bc41b2eb4ae4d2f6
AleFossati <alexandre.fossati@mercadolivre.com>
2023-01-04 15:24:44 -0300

    translating comments

--- static/js/index.js
+++ static/js/index.js
@@ -93,7 +93,7 @@ const proccessPayment = (selectedPaymentMethod, formData) => {
                     reject();
                 })
         } else if (selectedPaymentMethod === 'wallet_purchase') {
-            // wallet_purchase (Conta Mercado Pago) não precisa ser enviado pelo backend
+            // wallet_purchase (Mercado Pago Wallet) does not need to be sent to backend
             resolve();
             navToWallet();
         } else {
